1c7408a8ab461abcf579d70e2ef3d1ca0eeac8ff
sarah <49436342+sarah-adewale@users.noreply.github.com>
2022-04-30 02:24:53 +0100

    Add kanye album picker files

---
+++ js/main.js
@@ -0,0 +1,305 @@
+// add an event listener
+document.getElementById('gold').addEventListener('click', partyGold)
+document.getElementById('brown').addEventListener('click', partyBrown)
+document.getElementById('purple').addEventListener('click', partyPurple)
+document.getElementById('grey').addEventListener('click', partyGrey)
+document.getElementById('red').addEventListener('click', partyRed)
+document.getElementById('silver').addEventListener('click', partySilver)
+document.getElementById('orange').addEventListener('click', partyOrange)
+document.getElementById('lightBlue').addEventListener('click', partyLightBlue)
+document.getElementById('deepBlue').addEventListener('click', partyDeepBlue)
+document.getElementById('black').addEventListener('click', partyBlack)
+
+// declare a function
+function partyGold(){
+  document.querySelector('body').style.backgroundColor = 'rgb(240,183,53)'
+  document.querySelector('#gold').style.backgroundColor = 'rgb(240,183,53)'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#collegeDropout').style.display = 'block' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+}
+function partyBrown(){
+  document.querySelector('body').style.backgroundColor = 'rgba(75,36,2,1)'
+  document.querySelector('#brown').style.backgroundColor = 'rgba(75,36,2,1)'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'block' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+}
+function partyPurple(){
+  document.querySelector('body').style.backgroundColor = 'rgba(158,49,143)'
+  document.querySelector('#purple').style.backgroundColor = 'rgba(158,49,143)'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#red').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+  document.querySelector('#graduation').style.display = 'block'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+}
+function partyGrey(){
+  document.querySelector('body').style.backgroundColor = 'rgb(211,211,211)'
+  document.querySelector('#grey').style.backgroundColor = '	rgb(211,211,211)'
+  document.querySelector('#heartBreak').style.display = 'block'
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#red').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+}
+function partyRed(){
+  document.querySelector('body').style.backgroundColor = 'rgb(235, 13, 13)'
+  document.querySelector('#red').style.backgroundColor = 'rgb(235, 13, 13)'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+  document.querySelector('#darkTwisted').style.display = 'block'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+}
+function partySilver(){
+  document.querySelector('body').style.backgroundColor = 'rgba(198,202,209,1)'
+  document.querySelector('#silver').style.backgroundColor = 'rgba(198,202,209,1)'
+  document.querySelector('#yeezus').style.display = 'block'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#red').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+}
+function partyOrange(){
+  document.querySelector('body').style.backgroundColor = 'rgba(255,132,70,1)'
+  document.querySelector('#orange').style.backgroundColor = 'rgba(255,132,70,1)'
+  document.querySelector('#lifeOfPablo').style.display = 'block'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#red').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+}
+function partyLightBlue(){
+  document.querySelector('body').style.backgroundColor = 'rgba(162,180,205,1)'
+  document.querySelector('#lightBlue').style.backgroundColor = 'rgba(162,180,205,1)'
+  document.querySelector('#ye').style.display = 'block'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#red').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+}
+function partyDeepBlue(){
+  document.querySelector('body').style.backgroundColor = 'rgb(32,60,201)'
+  document.querySelector('#deepBlue').style.backgroundColor = 'rgb(32,60,201)'
+  document.querySelector('#jesusKing').style.display = 'block'
+  document.querySelector('#titleText').style.color = 'rgb(0, 0, 0)'
+  document.querySelector('#titleText').style.border = '5px solid rgb(0, 0, 0)'
+  document.querySelector('#pickAlbum').style.color = 'rgb(0, 0, 0)'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#donda').style.display = 'none'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#red').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#black').style.backgroundColor = 'white'
+
+
+}
+function partyBlack(){
+  document.querySelector('body').style.backgroundColor = 'rgb(14, 12, 12)'
+  document.querySelector('#black').style.backgroundColor = 'rgb(14, 12, 12)'
+  document.querySelector('#donda').style.display = 'block'
+  document.querySelector('#titleText').style.color = 'rgb(53, 57, 53)'
+  document.querySelector('#titleText').style.border = '5px solid rgb(53, 57, 53)'
+  document.querySelector('#pickAlbum').style.color = 'rgb(53, 57, 53)'
+  document.querySelector('#collegeDropout').style.display = 'none'
+  document.querySelector('#lateRegistration').style.display = 'none' 
+  document.querySelector('#graduation').style.display = 'none'
+  document.querySelector('#heartBreak').style.display = 'none'
+  document.querySelector('#darkTwisted').style.display = 'none'
+  document.querySelector('#yeezus').style.display = 'none'
+  document.querySelector('#lifeOfPablo').style.display = 'none'
+  document.querySelector('#ye').style.display = 'none'
+  document.querySelector('#jesusKing').style.display = 'none'
+  document.querySelector('#purple').style.backgroundColor = 'white'
+  document.querySelector('#gold').style.backgroundColor = 'white'
+  document.querySelector('#brown').style.backgroundColor = 'white'
+  document.querySelector('#grey').style.backgroundColor = 'white'
+  document.querySelector('#red').style.backgroundColor = 'white'
+  document.querySelector('#silver').style.backgroundColor = 'white'
+  document.querySelector('#orange').style.backgroundColor = 'white'
+  document.querySelector('#lightBlue').style.backgroundColor = 'white'
+  document.querySelector('#deepBlue').style.backgroundColor = 'white'
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('purple').onclick = partyPurple
+// document.getElementById('green').onclick = partyGreen
+// document.getElementById('blue').onclick = partyBlue
+// document.getElementById('red').onclick = partyRed
+
+// // call the function
+
+// function partyPurple(){
+//   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// function partyGreen(){
+//   document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+//   document.querySelector('body').style.color = 'black'
+// }
+
+// function partyBlue(){
+//   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// function partyRed(){
+//   document.querySelector('body').style.backgroundColor = 'rgba(255, 0, 0, 1)'
+//   document.querySelector('body').style.color = 'black'
+// }
